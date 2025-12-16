31fd3c6430a926c1f25583d59989e03f0e04f11e
Balu <balu.www.55@gmail.com>
2025-12-16 18:51:16 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,31 @@
+# CONVERSIONS :
+a = 15 
+b = 25 
+# here a is 15 and b is 25
+a = b
+b = a 
+# here we swapped a to b and b 
+# means we interchanged the element from a to b and b to a .
+print(a)
+# we can see the output tht a  assigned a value tht interchanged to b and b assigned value interchanged to a .
+
+# TYPE CONVERSIONS :
+
+# Ex what is type ?
+
+# in printing type we can get to know which data type .
+# simple code : 
+name = " balaji "
+print(type(name))
+
+# so we here printed name and it showing which data type it is printing in output .
+
+# TYPED CONVERSIONS :
+#simple code of typed conversions :
+age = 33 
+my_float = 39.5 # we have to assign a perfect variable like my_float like that if we assign float it will be error.
+age_float = float(age)
+print(age_float)
+
+# in this code we assigned some value  to variables to  age and float.
+# and we converted age variable to float using float() function and assigned it to age_float variable. 
