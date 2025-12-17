a1c5d518c293318775feab2daa07661cec61f6d2
Balu <balu.www.55@gmail.com>
2025-12-17 19:22:22 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,22 @@
+#  REVISE SESSION FROM 1 TO 4 
+# IN 1.PY :
+# we learnt what is coding and what is the use of it . 
+# we learnt about what is intrepreter and compiler .
+# we learnt what is python why python language is easy to learn .
+# we made a simple hello world using python.
+
+# 2.PY :
+# we learnt about what is variables , data types , constants  and operators in python .
+# what are the use of it 
+# how to assign a value and for which value which data type we must use .
+# and we learnt for which math operation which operator i should use .
+
+#3.PY 
+# we learnt abt input output manipulation.
+# how to input values from output .
+# and input output string manipulations also 
+
+#4.py 
+# we learn about conversions.
+# swapping .
+# and what is type and what are type conversions .
---
+++ 6.py
@@ -0,0 +1,22 @@
+# REPITITION:
+message = " Balu works very hard. "
+print(message *5)
+# repitition means to repeat the same string multiple times.
+
+# METHODS :
+message = " Balu works very hard. "
+print(message.lower())
+# lower() method converts all the characters in the string to lowercase.
+message = " Balu works very hard. "
+print(message.upper())
+# upper() method converts all the characters in the string to uppercase.
+
+# STRIP :
+message = "  Balu works very hard.  "
+print(message.strip()*2) 
+# strip() method removes any leading (spaces at the beginning) and trailing (spaces at the ending).
+
+# REPLACE :
+message = " Balu works very hard. "
+print(message.replace("Balu","ramu"))
+
