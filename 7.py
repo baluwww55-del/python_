0ad142bfbb2953be04988229264dd5b3cc355249
Balu <balu.www.55@gmail.com>
2026-01-09 21:34:12 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,23 @@
+# FINDING LENGTH :
+
+name = " S Balaji "
+print(len(name)) # it will count the number of characters in the string including spaces.
+
+# INDEX IN STRING :
+name = " S Balaji "
+print(name[4]) # it will print the character at index.
+
+# negative indexing :
+print(name[-2]) # it will print the character from the end of the string.
+
+# positive indexing :
+print(name[0]) # it will print the first character of the string.
+
+# SLICING IN STRING ;
+print(name[:2]) # it will print the characters from the start to index 2 (excluding index 2).
+print(name[2:5]) # it will print the characters from index 2 to index 5 (excluding index 5).
+print(name[:5])
+
+# SKIP THE LIST :
+print(name[::2]) # it will print every 5th character in the string.
+
