b7595514792fe7ba30d36914f0551e5aff3da4d0
Balu <balu.www.55@gmail.com>
2026-03-21 01:55:22 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,33 @@
+# OPERATORS IN PYTHON :
+# ARITHMETIC OPERATORS :
+a = 10
+b = 20 
+print(a+b) #addition 
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a%b) #modulus
+print(a**b) #exponentiation
+
+# ASSIGNMENT OPERATORS :
+a = 10
+a += 5 # a = a + 5
+print(a) # it will print 15 because we added 5 to a.
+# SO ASSIGNMENT OPERATORS ARE USED TO ASSIGN THE VALUES TO A VARIABLE AND ALSO TO PERFORM SOME OPERATIONS ON THE VARIABLE.
+
+# COMPARISON OPERATORS :
+a = 10
+b = 20  
+print(a == b) # it will print False because a is not equal to b.
+print(a != b) # it will print True because a is not equal to b.
+print(a < b) # it will print True because a is less than b.
+print(a > b) # it will print False because a is not greater than b.
+print(a <= b) # it will print True because a is less than or equal to b.
+print(a >= b) # it will print False because a is not greater than or equal to b.
+
+#LOGICAL OPERATORS :(AND OR NOT)
+a = 10
+b = 20
+print(a > 5 and b < 30) # it will print True because both conditions are true.
+print(a > 15 or b < 30) # it will print True because one of the conditions is true.
+print(not(a > 5)) # it will print False because a is greater than 5 and not operator negates the condition.
