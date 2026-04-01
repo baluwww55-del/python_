36836d422d0180bacd605828978c3890169397b9
Balu <balu.www.55@gmail.com>
2026-04-02 00:14:40 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,21 @@
+# LOOPS IN PYTHON :
+# 1. For Loop :
+# for loop is used to iterate over a sequence (like a list, tuple, string) or other iterable objects.
+# Syntax :
+i = 0
+for i in range(5):
+    print(i)
+
+# this is simple example of for loop in python which will print number from 0 to 4.
+
+# 2. While Loop :
+# while loop is used to execute a block of code repeatedly as long as a given condition is true.
+# Syntax :
+i = 0
+while i < 5:
+    print(i)
+    i += 1  
+
+# this is simple example of while loop in python which will print number from 0 to 4.
+
+# these two example of loops are for the print numbers from 0 to 4 using for loop and while loop which is used in python programming language.
