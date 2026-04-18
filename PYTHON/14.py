bfbe198b06bbf0fb8608647600d24954a4c1c5d6
Balu <balu.www.55@gmail.com>
2026-04-18 15:04:10 +0530

    Refactor marks evaluation code with comments and fixes
    Updated the marks evaluation code with comments for clarity and fixed a grade condition for scores of 90.

---
+++ PYTHON/14.py
@@ -0,0 +1,17 @@
+# projects using python 
+# SIMPLE CODE USING CODITIONAL STATEMENT USING PYTHON PROGRAMMING LANGUAGE :
+marks = int(input("Enter your marks: "))
+
+if marks < 0 or marks > 100: # USED IF STATEMENT AND USED LOGICAL OPERATOR TO MAKE CODE EFFIECIENT .
+    print("Invalid marks")
+elif marks >= 90:          # FIXED A GRADE TO GIVE THE STUDENTS WHO SCORED 905
+    print("Grade A+ - Topper mindset")
+elif marks >= 75:           # THE STUDENT WHO GOT AVERAGE  GIVEN GRADE `A 
+    print("Grade A - Strong work")
+elif marks >= 50:            # THE STUDENT WHO GOT 50 / DESCENT SCORE GIVEN GRADE B 
+    print("Grade B - Decent, push harder")
+else:                       # AT THE END THE STUDENT WHO GOT LESS SCORE WILL BE FAILED USING ELSE :
+    print("Fail - Reality check")
+
+# this is the simple marks card grade evaluator using python 
+# SO THIS IS THE CODE WHICH IS KNOWN AS MARKS CARD USING PYTHON CONDTIONAL SIMPLE STATEMENT .
